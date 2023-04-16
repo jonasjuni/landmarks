0e35a479a9ca1b0ad22496f3da22d33711b1acb0
Jonas Cerqueira da Silva Junior <jonas.developer@live.com>
2023-04-16 18:52:47 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jonas Cerqueira da Silva Junior on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
