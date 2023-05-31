3e4d8f99b8a6441fb6385c660973b471e2a2be55
Jonas Cerqueira da Silva Junior <jonas.developer@live.com>
2023-05-31 20:03:19 -0300

    Tutorial final

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Jonas Cerqueira da Silva Junior on 16/04/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
