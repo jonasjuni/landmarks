1022f487441134f970bd6e8973d9ce665bd7aea7
Jonas Cerqueira da Silva Junior <jonas.developer@live.com>
2023-08-20 19:05:04 -0300

    Working with UI controls tutorial

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
+    
     var body: some View {
         NavigationStack {
             List {
@@ -24,8 +26,20 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
             .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+//                    .environmentObject(modelData) //Why???
+            }
         }
     }
 }
---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Jonas Cerqueira da Silva Junior on 20/08/23.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .font(.title)
+                    .bold()
+                Text("Notifications: \(profile.prefersNotifications ? "On" : "Off")")
+                Text("Seasonal Photos: \(profile.seaonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    ScrollView(.horizontal){
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
+                
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
+    }
+}
